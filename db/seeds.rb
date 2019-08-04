cf36c687a61fcf72f9e2caed5091d54dc461328b
Ian Hankin <ian.hankin@gmail.com>
2019-08-04 15:35:59 +0000

    Seeds and Home Page Start

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,21 @@
 User.create!(name: "user1", password: "glbrcpass", password_confirmation: "glbrcpass")
 User.create!(name: "user2", password: "glbrcpass", password_confirmation: "glbrcpass")
 User.create!(name: "user3", password: "glbrcpass", password_confirmation: "glbrcpass")
+
+Application.create!(name: "Google", description: "Search Engine", color: "Red",
+  default: true, link: "http://www.google.com")
+Application.create!(name: "Wisc", description: "UW homepage", color: "Blue",
+  default: false, link: "http://www.wisc.edu")
+Application.create!(name: "GLBRC", 
+  description: "Great Lakes Bioenergy Research Center", color: "Yellow", 
+  default: true, link: "http://www.glbrc.org")
+Application.create!(name: "WEI", description: "Wisconsin Energy Institute", 
+  color: "Green", default: false, link: "https://energy.wisc.edu/")
+Application.create!(name: "Twitter", description: "Twitter", color: "Purple",
+  default: false, link: "https://twitter.com/")
+  
+User.all.each do |user|
+  Application.all.each do |app|
+    Preference.create(user_id: user.id, application_id: app.id, active: app.default)
+  end
+end
